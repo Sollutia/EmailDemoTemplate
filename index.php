f2f5143785fdb06deddba774c94813cd4566cc7c
Adria-Sollutia <adria.wolfvenire@outlook.es>
2022-07-08 13:15:12 +0200

    Primer Commit

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="https://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta name="x-apple-disable-message-reformatting">
+  <!--[if !mso]><-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!--<![endif]-->
+  <!--ONLY FOR INTERNET EXPLORER 9 OR LOWER, IS MORE OPTIONAL,MANTAIN THIS CONDITIONALS!-->
+  <title></title>
+  <!--[if mso]>
+  <style type="text/css">
+      table {border-collapse:collapse;border:0;border-spacing:0;margin:0;}
+      div, td {padding:0;}
+      div {margin:0 !important;}
+  </style>
+  <noscript>
+  <xml>
+      <o:OfficeDocumentSettings>
+          <o:PixelsPerInch>96</o:PixelsPerInch>
+      </o:OfficeDocumentSettings>
+  </xml>
+  </noscript>
+  <![endif]-->
+  <style type="text/css">
+@media only screen and (max-width: 350px) {
+  table.body h1 {
+    font-size: 28px !important;
+    margin-bottom: 10px !important;
+  }
+
+  table.body p,
+table.body ul,
+table.body ol,
+table.body td,
+table.body span,
+table.body a {
+    font-size: 16px !important;
+  }
+
+  table.body .wrapper,
+table.body .article {
+    padding: 10px !important;
+  }
+
+  table.body .content {
+    padding: 0 !important;
+  }
+
+  table.body .container {
+    padding: 0 !important;
+    width: 100% !important;
+  }
+
+  table.body .main {
+    border-left-width: 0 !important;
+    border-radius: 0 !important;
+    border-right-width: 0 !important;
+  }
+
+  table.body .btn table {
+    width: 100% !important;
+  }
+
+  table.body .btn a {
+    width: 100% !important;
+  }
+
+  table.body .img-responsive {
+    height: auto !important;
+    max-width: 100% !important;
+    width: auto !important;
+  }
+}
+@media all {
+  .ExternalClass {
+    width: 100%;
+  }
+
+  .ExternalClass,
+.ExternalClass p,
+.ExternalClass span,
+.ExternalClass font,
+.ExternalClass td,
+.ExternalClass div {
+    line-height: 100%;
+  }
+
+  .apple-link a {
+    color: inherit !important;
+    font-family: inherit !important;
+    font-size: inherit !important;
+    font-weight: inherit !important;
+    line-height: inherit !important;
+    text-decoration: none !important;
+  }
+
+  #MessageViewBody a {
+    color: inherit;
+    text-decoration: none;
+    font-size: inherit;
+    font-family: inherit;
+    font-weight: inherit;
+    line-height: inherit;
+  }
+
+  .btn-primary table td:hover {
+    background-color: #34495e !important;
+  }
+
+  .btn-primary a:hover {
+    background-color: #34495e !important;
+    border-color: #34495e !important;
+  }
+}
+</style>
+  </head>
+  <body style="background-color: #f6f6f6; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;">
+    
+  <div role="article" aria-roledescription="email" lang="en" style="-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;max-width:680px;margin:auto;background-color:#ffffff;">  
+      <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f6f6f6;max-width:680px; width: 100%;margin:auto;" width="100%" bgcolor="#f6f6f6">
+          <tr>
+            <td class="container" style="font-family: sans-serif; font-size: 14px; vertical-align: top; display: block; max-width: 660px; padding: 10px; width: 96%; margin: 0 auto;" width="580" valign="top">
+            <!--[if mso]>
+                <table role="presentation" align="center" style="width:660px;">
+                <tr>
+                <td style="padding:20px 0;">
+              <![endif]-->  
+              <div class="content" style="width:96%;box-sizing: border-box; display: block; margin: 0 auto; max-width:660px; margin:20px auto;">
+
+                <!-- START CENTERED WHITE CONTAINER -->
+                <table role="presentation" class="main" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background: #ffffff; border-radius: 3px; width: 100%;border:0;border-spacing:0;" width="100%">
+
+                  <!-- START MAIN CONTENT AREA -->
+                  <tr>
+                    <td class="wrapper" style="font-family: sans-serif; font-size: 14px; vertical-align: top; box-sizing: border-box; padding: 20px;" valign="top">
+                      <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
+                        <tr>
+                          <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;" valign="top">
+                            <img src="#" alt="Area-Logotipo" width="200" height="52" style="border: none; -ms-interpolation-mode: bicubic; max-width: 100%; margin: 0 auto 1.5rem auto; display: block;">
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="font-family: sans-serif; font-size: 16px; vertical-align: top;" valign="top">
+                              <p style="font-family: sans-serif; font-weight: normal; margin: 0; margin-bottom: 15px;">[Content goes Here]</p>
+                              <p style="margin:0;font-family:Arial,sans-serif;"><a href="#" style="background: #ffffff; border: 2px solid #8dc1d6; text-decoration: none; padding: 10px 25px; color: #000000; border-radius: 4px; display:inline-block; mso-padding-alt:0;text-underline-color:#ffffff"><!--[if mso]><i style="letter-spacing: 25px;mso-font-width:-100%;mso-text-raise:20pt">&nbsp;</i><![endif]-->
+                                <span style="mso-text-raise:10pt;font-weight:bold;">Example Button</span><!--[if mso]><i style="letter-spacing: 25px;mso-font-width:-100%">&nbsp;</i><![endif]--></a></p>
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                <!-- END MAIN CONTENT AREA -->
+                </table>
+                <!-- END CENTERED WHITE CONTAINER -->
+
+                <!-- START FOOTER -->
+                <div class="footer" style="clear: both; margin-top: 10px; text-align: center; width: 100%;">
+            <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
+              <tr>
+                <td class="content-block powered-by" colspan="2" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; color: #999999; font-size: 11px; text-align: center;" valign="top" align="center">
+                <a href="https://www.casesnoves.es" style="color: #999999; font-size: 11px; text-align: center; font-weight: bold; text-decoration: none;">www.Example.es</a><br>
+                <span class="apple-link" style="color: #999999; font-size: 11px; text-align: center;">C/ Falsa 123 Springfield (OR)</span>
+                </td>
+              </tr>
+              <tr>
+                <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; color: #999999; font-size: 13px; text-align: center;" valign="top" align="center">
+                <table role="presentation" style="width:100%;border:0;border-spacing:0;">
+                  <tr>
+                    <td style="padding:3px;text-align:center;">
+                      <a class="spaced-link" href="tel:+34965885309" style="text-decoration: underline; padding: 12px 12px 12px 12px; color: #999999; text-align: center;">Tel.: +34000000000</a>
+                    </td>
+                    <td style="padding:3px;text-align:center;">
+                      <a class="spaced-link" href="tel:+34676010171" style="text-decoration: underline; padding: 12px 12px 12px 12px; color: #999999; text-align: center;">Móvil: +34000000000</a>
+                    </td>
+                    <td style="padding:3px;text-align:center;">
+                      <a class="spaced-link" href="mailto:hola@casesnoves.es" style="text-decoration: underline; padding: 12px 12px 12px 12px; color: #999999; text-align: center;">Example@example.es</a>
+                    </td>
+                    <td style="padding:3px;text-align:center;">
+                      <a class="spaced-link" href="#" style="text-decoration: underline; padding: 12px 12px 12px 12px; color: #999999; text-align: center;">Llámanos con Skype</a>
+                    </td>
+                  </tr>
+                </table>
+                
+                
+               
+               
+                </td>
+                </tr>
+            </table>
+                  <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
+                    <tr>
+                      <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; color: #999999; font-size: 11px; text-align: center;" valign="top" align="center">
+                        Tanto este mensaje como todos los posibles documentos adjuntos al mismo son confidenciales y están dirigidos exclusivamente a los destinatarios de los mismos. Por favor, si Ud. no es uno de dichos destinatarios, notifíquenos este hecho y elimine el mensaje de su sistema. Queda prohibida la copia, difusión o revelación de su contenido a terceros sin el previo consentimiento por escrito de <a href="#" style="text-decoration: underline; color: #999999; font-size: 11px; text-align: center;">www.example.es</a> . En caso contrario, vulnerará la legislación vigente.
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+                <!-- END FOOTER -->
+
+              </div>
+            </td>
+  
+          </tr>
+        </table>
+    </div>
+  </body>
+</html>
