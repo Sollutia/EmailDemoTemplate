6997084f59bff361ce062c4cb7520ae38b74fc10
Adria-Sollutia <adria.wolfvenire@outlook.es>
2022-07-12 11:52:11 +0200

    [add] plantilla basica y diseño alternativo

---
+++ alternative-templates/plantilla.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="https://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta name="x-apple-disable-message-reformatting">
+  <!--[if !mso]><-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!--<![endif]-->
+  <!--ONLY FOR INTERNET EXPLORER 9 OR LOWER, IS MORE OPTIONAL,MANTAIN THIS CONDITIONALS!-->
+  <title></title>
+  <!--[if mso]>
+  <style type="text/css">
+      table {border-collapse:collapse;border:0;border-spacing:0;margin:0;}
+      div, td {padding:0;}
+      div {margin:0 !important;}
+  </style>
+  <noscript>
+  <xml>
+      <o:OfficeDocumentSettings>
+          <o:PixelsPerInch>96</o:PixelsPerInch>
+      </o:OfficeDocumentSettings>
+  </xml>
+  </noscript>
+  <![endif]-->
+  <style type="text/css">
+      @media screen and (max-width: 350px) {
+        table.body h1 {
+            font-size: 28px !important;
+            margin-bottom: 10px !important;
+        }
+        table.body p,
+        table.body ul,
+        table.body ol,
+        table.body td,
+        table.body span,
+        table.body a {
+            font-size: 16px !important;
+        }
+        table.body .wrapper,
+        table.body .article {
+            padding: 10px !important;
+        }
+
+        table.body .content {
+            padding: 0 !important;
+        }
+
+        table.body .container {
+            padding: 0 !important;
+            width: 100% !important;
+        }
+
+        table.body .main {
+            border-left-width: 0 !important;
+            border-radius: 0 !important;
+            border-right-width: 0 !important;
+        }
+
+        table.body .btn table {
+            width: 100% !important;
+        }
+
+        table.body .btn a {
+            width: 100% !important;
+        }
+
+        table.body .img-responsive {
+            height: auto !important;
+            max-width: 100% !important;
+            width: auto !important;
+        }
+      }
+      @media screen and (min-width: 351px) and (max-width: 460px) {
+        .three-col .column {
+            max-width: 50% !important;
+        }
+    }
+    @media screen and (max-width: 460px) {
+        .two-col .column {
+            max-width: 100% !important;
+        }
+        .two-col img {
+            width: 100% !important;
+        }
+    }
+    @media screen and (min-width: 461px) {
+        .three-col .column {
+            max-width: 33.3% !important;
+        }
+        .two-col .column {
+            max-width: 50% !important;
+        }
+        .sidebar .small {
+            max-width: 16% !important;
+        }
+        .sidebar .large {
+            max-width: 84% !important;
+        }
+    }
+  </style>
+</head>
+<body style="margin:0;padding:0;word-spacing:normal;background-color:#f6f6f6;">
+    <div role="article" aria-roledescription="email" lang="en" style="-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;background-color:#ffffff;">
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f6f6f6;max-width:680px; width: 100%;margin:auto;" width="100%" bgcolor="#f6f6f6">
+            <tr>
+                <td align="center">
+                <!--[if mso]>
+                    <table role="presentation" align="center" style="width:660px;">
+                    <tr>
+                    <td style="padding:20px 0;">
+                <![endif]-->
+                    <div class="outer" style="width:96%;max-width:660px;margin:20px auto;">
+                        <table role="presentation" style="width:100%;border:0;border-spacing:0;">
+                        <!-- Usar estas filas para estructurar contenido si es necesario añadir imagenes o fondos con detalles, a partir de aqui generar el resto, se puede cambiar el tamaño de la fuente en los rows
+                            <tr>
+                                <td style="padding:10px 10px 0px 10px;font-family:Arial,sans-serif;font-size:24px;line-height:28px;font-weight:bold;background-color:#0f1a1e;">
+                                    <img src="#" alt="Logotipo de Empresa" width="658" alt="" style="width:100%;height:auto;">
+                                </td>
+                            </tr>
+
+                            Usar esta fila para la imagen, tener cuidado con el formato para que se adapte a la celdilla.
+                            <tr>
+                                <td style="font-family:Arial,sans-serif;font-size:24px;line-height:28px;font-weight:bold; background-color:#0f1a1e;">
+                                    <p style="padding:20px;text-align:center;color:white;">Ejemplo</p>
+                                </td>
+                            </tr>
+                        -->
+                            <tr>
+                                <td style="padding:10px 10px 0px 10px;font-family:Arial,sans-serif;font-size:24px;line-height:28px;font-weight:bold;background-color:#0f1a1e;">
+                                    <img src="#" alt="Logotipo de Empresa" width="658" alt="" style="width:100%;height:auto;">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="font-family:Arial,sans-serif;font-size:24px;line-height:24px;font-weight:bold; background-color:#0f1a1e;">
+                                    <p style="padding:20px;text-align:center;color:white;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, omnis. Voluptates laudantium fugit, tempore aut excepturi vitae est. Corrupti, distinctio corporis? Magnam officiis nobis laborum voluptas repudiandae error aut officia.</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <!-- START FOOTER  Editar a conveniencia-->
+                                <table role="presentation" style="width:100%;border:0;border-spacing:0;">
+                                    <tr>
+                                        <td align= center>
+                                            <img src="#" alt="logo Corporativo" width="165px" height="auto" style="padding-top: 15px;margin:0 auto;paddig-right:5px">
+                                        </td>
+                                        <td style="padding-right:5px;width:50%">
+                                            <p>Preferencias de correo electrónico</p>
+                                            <p>Establece tus preferencias de comunicación para que podamos enviarte correos electrónicos importantes.</p>
+                                            <br>
+                                            <p><a href="#">Anular suscripción</a>  <a href="#">Actualizar mis preferencias</a></p>
+                                        </td>
+                                    </tr>
+                                    <div class="footer" style="clear: both; margin-top: 10px; text-align: center; width: 100%;">
+                                        <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
+                                            <tr>
+                                                <td class="content-block powered-by" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; color: #999999; font-size: 12px; text-align: center;" valign="top" align="center">
+                                                    Powered by <a href="#" style="color: #999999; font-size: 12px; text-align: center; text-decoration: none;">Enlace</a>.
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <tdclass="content-block powered-by" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; color: #999999; font-size: 12px; text-align: center;" valign="top" align="center">
+                                                    <a class="spaced-link" href="tel:+34965885309" style="text-decoration: underline; padding: 12px 12px 12px 12px; color: #999999; font-size: 11px; text-align: center;">Tel.: +34 96 588 53 09</a>
+                                                    <a class="spaced-link" href="tel:+34965885309" style="text-decoration: underline; padding: 12px 12px 12px 12px; color: #999999; font-size: 11px; text-align: center;">Tel.: +34 96 588 53 09</a>
+                                                    <a class="spaced-link" href="tel:+34965885309" style="text-decoration: underline; padding: 12px 12px 12px 12px; color: #999999; font-size: 11px; text-align: center;">Tel.: +34 96 588 53 09</a>
+                                                    <a class="spaced-link" href="tel:+34965885309" style="text-decoration: underline; padding: 12px 12px 12px 12px; color: #999999; font-size: 11px; text-align: center;">Tel.: +34 96 588 53 09</a>
+                                                </td>
+                                            </tr>  
+                                            <tr>
+                                                <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; color: #999999; font-size: 12px; text-align: center;" valign="top" align="center">
+                                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">Company Inc, False Street 123, Springfield OR 94102</span>
+                                                <br> Don't like these emails? <a href="http://i.imgur.com/CScmqnj.gif" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">Unsubscribe</a>.
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                            <td class="content-block powered-by" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; color: #999999; font-size: 12px; text-align: center;" valign="top" align="center">
+                                                Powered by <a href="#" style="color: #999999; font-size: 12px; text-align: center; text-decoration: none;">Enlace</a>.
+                                           </td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                    <!-- END FOOTER -->
+                                </table>
+                            </tr>
+                        </table>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
